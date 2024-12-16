a3cae91d339fe934c8df8962323648e6ff91660c
fiezaeza <fiezanm0404@gmail.com>
2024-12-16 09:16:01 +0700

    initial commit

---
+++ MessageService.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System.Threading.Tasks;
+
+namespace wamessage
+{
+    public class MessageService
+    {
+        private readonly string _baseUrl = "https://waapi.app/api/v1/instances/31507/client/action/send-message";
+        private readonly string _token = "57M7LT0IhZy2fWTaBBiIb0C9ZKpHYs3sx7bSQWDH7f5dc48f";
+
+        public async Task<string> SendMessageAsync(string message, string chatId)
+        {
+            var option = new RestClientOptions(_baseUrl);
+            var client = new RestClient(option);
+            var request = new RestRequest();
+
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("authorization", $"Bearer {_token}");
+
+            var body = new
+            {
+                message = message,
+                chatId = $"{chatId}@c.us"
+            };
+
+            request.AddJsonBody(body);
+
+            var response = await client.PostAsync(request);
+
+            if (response != null)
+            {
+                return response.Content;
+            }
+            else
+            {
+                return "No Message";
+            }
+        }
+    }
+}
